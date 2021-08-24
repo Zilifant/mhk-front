403c009a5ba11bdd3d98c0097c6c4cd7ea56fd14
Scott Silsbe <scott.silsbe@gmail.com>
2021-08-24 13:09:38 -0400

    added timer with inc/dec functionality to setup component; restructured setup component into grid layout

--- src/components/lobby/main/nogame/Setup.jsx
+++ src/components/lobby/main/nogame/Setup.jsx
@@ -1,5 +1,6 @@
 import React, {
-  useContext
+  useContext,
+  useState
 } from 'react';
 import { SocketContext } from '../../../../context/contexts';
 import { useGame } from '../../../../hooks/game-hook';
@@ -15,11 +16,48 @@ const Setup = ({
 
   const { socket } = useContext(SocketContext);
 
+  const [timer, setTimer] = useState({
+    isOn: false,
+    minReached: true,
+    maxReached: false,
+    min: 1,
+    max: 5,
+    val: 1
+  });
+
   const {
     startGameHandler,
     toggleHandler,
     chooseTimerHandler,
   } = useGame(socket);
+
+  function inc() {
+    if (timer.val < timer.max) setTimer({
+      ...timer,
+      ...{
+        val: ++timer.val,
+        minReached: false,
+        isOn: true
+      }});
+
+    if (timer.val === timer.max) setTimer({...timer, ...{maxReached: true}});
+    const duration = !timer.isOn ? 'off' : timer.val;
+    return chooseTimerHandler(duration);
+  };
+
+  function dec() {
+    if (timer.val > timer.min) setTimer({...timer, ...{val: --timer.val, maxReached: false}});
+    if (timer.val === timer.min) setTimer({...timer, ...{minReached: true}});
+    const duration = !timer.isOn ? 'off' : timer.val;
+    return chooseTimerHandler(duration);
+  };
+
+  function offOn() {
+    setTimer({...timer, ...{isOn: !timer.isOn}});
+    const duration = timer.isOn ? 'off' : timer.val;
+    console.log(duration);
+    return chooseTimerHandler(duration);
+  }
 
   const advRolesLeader = () => (<>
     <div className='advrole-wrapper'>
@@ -57,67 +95,65 @@ const Setup = ({
     >ACCOMPLICE</div>
   </>);
 
-  const timerLeader = () => (<>
-    <section className='section-wrapper'>
-      <div className='text'>
-        <p>Round Timer (Minutes)</p>
-      </div>
-      <div className='timer-buttons'>
-        {gameSettings.timer.durationOpts.map((opt, i) => (
-          <Button
-            key={i}
-            className={`timer ${opt === gameSettings.timer.duration ? 'selected' : 'unselected'}`}
-            onClick={() => chooseTimerHandler(opt)}
-            disabled={false}
-          >
-            {opt}
-          </Button>
-        ))}
-      </div>
-    </section>
-  </>);
+  const timerLeader = () => (
+    <div className='timer-wrap'>
+      <div className='timer title'>Round Timer</div>
+      <Button
+        className={`timer toggle ${timer.isOn ? 'on' : 'off'}`}
+        onClick={offOn}
+        disabled={false}
+      >{timer.isOn ? 'X' : 'O'}</Button>
+      <Button
+        className='timer dec'
+        onClick={dec}
+        disabled={timer.minReached || !timer.isOn}
+      >-</Button>
+      <div className={`timer value ${timer.isOn ? 'on' : 'off'}`}>{`0${timer.val}:00`}</div>
+      <Button
+        className='timer inc'
+        onClick={inc}
+        disabled={timer.maxReached || !timer.isOn}
+      >+</Button>
+    </div>
+  );
 
-  const timerBasic = () => (<>
-    <section className='section-wrapper'>
-      <div className='text'>
-        <p>Round Timer: {gameSettings.timer.on ? `${gameSettings.timer.duration} Minutes` : 'OFF'}</p>
-      </div>
-    </section>
-  </>);
+  const timerBasic = () => (
+    <div className='timer value'>
+      <p>Round Timer: {gameSettings.timer.on ? `${gameSettings.timer.duration} Minutes` : 'OFF'}</p>
+    </div>
+  );
 
   return (
-    <Container className={'setup'}>
+    <Container className={`setup ${iAmLeader ? 'leader' : 'notleader'}`}>
 
-      <section className='section-wrapper'>
-        <div className='text'>
-          <p>{lobby.startGameText(iAmLeader)}</p>
-        </div>
-        {iAmLeader && <Button
+      {iAmLeader &&
+      <div className='setup-section stbtn'>
+        <Button
           onClick={startGameHandler}
           disabled={!lobby.canStart()}
-        >Start Game</Button>}
-      </section>
+        >Start Game</Button>
+      </div>}
 
-      <section className='section-wrapper'>
-        <div className='text'>
-          <p>{lobby.text.NOTE_CHOOSE_GHOST(iAmLeader)}</p>
-        </div>
-        <div className='advrole ghost'>
-          {gameSettings.assignedToGhost?.slice(0,-5) || 'RANDOM'}
-        </div>
-      </section>
+      <div className='setup-section sttxt txt'>
+        <p>{lobby.startGameText(iAmLeader)}</p>
+      </div>
 
-      <section className='section-wrapper'>
-        <div className='text'>
-          <p>{lobby.advRolesEnText(iAmLeader)}</p>
-        </div>
-        <div className='text'>
-          <p>{lobby.advRolesRecText()}</p>
-        </div>
+      <div className='setup-section ghost txt'>
+        <p>{lobby.text.NOTE_CHOOSE_GHOST(iAmLeader)} Ghost: <span className='advrole ghost'>{gameSettings.assignedToGhost?.slice(0,-5) || 'RANDOM'}</span></p>
+      </div>
+
+      <div className='setup-section wabtn'>
         {iAmLeader ? advRolesLeader() : advRolesBasic()}
-      </section>
+      </div>
 
-      {iAmLeader ? timerLeader() : timerBasic()}
+      <div className='setup-section watxt txt'>
+        <p>{lobby.advRolesRecText()}</p>
+        <p>{lobby.advRolesEnText(iAmLeader)}</p>
+      </div>
+
+      <div className='setup-section timer'>
+        {iAmLeader ? timerLeader() : timerBasic()}
+      </div>
 
     </Container>
   );

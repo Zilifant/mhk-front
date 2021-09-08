c197152f933985290ae59a7cd0d36d2d69c1fdd2
Scott Silsbe <scott.silsbe@gmail.com>
2021-09-08 16:52:21 -0400

    changes to chat css; removed newmessage button

--- src/components/lobby/chat/NewMessage.jsx
+++ src/components/lobby/chat/NewMessage.jsx
@@ -11,26 +11,43 @@ const NewMessage = ({
   myLobby, onChange, messageText, submitHandler
 }) => {
 
+  const isValid = validate(messageText,
+    [
+      VALIDATOR_REQUIRE(),
+      VALIDATOR_MAXLENGTH(280),
+      VALIDATOR_BANNED_CHARS(['^','_','<','>'])
+    ]
+  );
+
+  const hasBannedValue = validate(messageText,
+    [
+      VALIDATOR_MAXLENGTH(280),
+      VALIDATOR_BANNED_CHARS(['^','_','<','>'])
+    ]
+  );
+
+  const inputStyle = () => {
+    const base = 'input new-message-input';
+    const valid = isValid ? 'valid' : 'invalid';
+    const banned = hasBannedValue ? 'notbanned' : 'banned';
+    return `${base} ${valid} ${banned}`;
+  };
+
   return (
     <React.Fragment>
       {myLobby &&
         <div className='new-message-wrapper'>
           <form className='form new-message-form'>
             <input
-              className='input new-message-input'
+              className={inputStyle()}
               placeholder='Message'
               onChange={onChange}
               value={messageText}
             ></input>
             <Button
-              disabled={!validate(messageText,
-                [
-                  VALIDATOR_REQUIRE(),
-                  VALIDATOR_MAXLENGTH(280),
-                  VALIDATOR_BANNED_CHARS(['^','_','<','>'])
-                ]
-              )}
+              disabled={!isValid}
               onClick={submitHandler}
+              className='new-message'
             >
               Send
             </Button>

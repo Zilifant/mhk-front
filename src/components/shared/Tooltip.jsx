9fa78fe46ed30dd7fe9f85836c919ef49f4ee39b
Scott Silsbe <scott.silsbe@gmail.com>
2021-09-27 15:43:38 -0400

    refactored smd css

--- src/components/shared/Tooltip.jsx
+++ src/components/shared/Tooltip.jsx
@@ -1,23 +1,11 @@
 
 import { parseSMDLines, renderStyledLines } from '../../util/styled-text';
+import { tooltipText } from '../../util/tooltip-text';
 import '../../styles/tooltips.scss';
 
-const tooltips = {
-  waitMorePlayers: 'At least ^_k_4^ players are needed to start.<^_k_5^ or more are recommended.',
-  advRoles: 'At least ^_k_5^ players are needed to use the ^_k_Witness^ and ^_k_Accomplice^ roles.<^_k_6^ or more are recommended.<For the most balanced game, use both roles (or neither).<>f>Using only the ^_k_Witness^ may make the game harder for the ^_k_Killer^.<>f>Using only the ^_k_Accomplice^ may make things harder for the ^_k_Hunters^ and the ^_k_Ghost^.',
-  timerSetup: '^_e_Optional:^ Limit how many ^_k_minutes^ players have for discussion each round.<>f>The round will continue even after the timer runs out. The lobby leader may choose how strictly to enforce the limit.',
-  assignGhost: '^_e_Optional:^ Assign a player to be the ^_k_Ghost^.',
-  youAreLeader: 'You are the lobby leader.',
-  transferLeader: 'Transfer ^_k_leadership^ to this user.',
-  copyUrl: 'Copy lobby URL',
-  hideShowName: 'Hide/show lobby name',
-  streamingMode: 'When ^_k_Streaming Mode^ is active, the Lobby\'s ID and URL will be hidden by default.',
-  clearGameWarn: '^_w_End the game^ early and return to the lobby. ^_i_Are you sure?'
-};
-
 function renderTooltip(tip, side, opts) {
-  if (!tooltips[tip]) return null;
-  const parsedTip = parseSMDLines({lines: tooltips[tip]});
+  if (!tooltipText[tip]) return null;
+  const parsedTip = parseSMDLines({lines: tooltipText[tip]});
   return (
     <div className={(`ttip ${side} ${opts || ''}`).trim()}>
       {renderStyledLines(parsedTip)}

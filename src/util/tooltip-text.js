55c4a52d06e537bba8c4d71e5749813c6987f556
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-08 17:11:42 -0500

    updated tooltip formatting and text

--- src/util/tooltip-text.js
+++ src/util/tooltip-text.js
@@ -1,9 +1,9 @@
 // Tooltip text
 
 export const tooltipText = {
-  waitMorePlayers: 'At least ^_k_4^ players are needed to start.<^_k_5^ or more are recommended.',
-  advRoles: 'At least ^_k_5^ players are needed to use the ^_kw_Witness^ and ^_ka_Accomplice^ roles.<^_k_6^ or more are recommended.<For the most balanced game, use both roles (or neither).<>i>Using only the ^_kw_Witness^ may make the game harder for the ^_kk_Killer^.<>i>Using only the ^_ka_Accomplice^ may make things harder for the ^_kh_Hunters^ and the ^_kg_Ghost^.',
-  timerSetup: '^_e_Optional:^ Limit how many ^_k_minutes^ players have for discussion each round.<>f>The round will continue even after the timer runs out. The lobby leader may choose how strictly to enforce the limit.',
+  waitMorePlayers: 'At least ^_k_4^ players are needed to start.<^_k_5^ or more players are recommended for the best experience.',
+  advRoles: 'At least ^_k_5^ players are needed to use the ^_kw_Witness^ and ^_ka_Accomplice^ roles. ^_k_6^ or more are recommended.<For the most balanced game use both roles or use neither of the advanced roles.<>i>Using only the ^_kw_Witness^ may make the game harder for the ^_kk_Killer^. Using only the ^_ka_Accomplice^ may make things harder for the ^_kh_Hunters^ and the ^_kg_Ghost^.',
+  timerSetup: '^_e_Optional:^ Limit how many ^_k_minutes^ players have for discussion each round.<>i>The round will continue even after the timer runs out. The lobby leader may choose how strictly to enforce the limit.',
   assignGhost: '^_e_Optional:^ Assign a player to be the ^_kg_Ghost^.',
   youAreLeader: 'You are the lobby leader.',
   transferLeader: 'Transfer ^_k_leadership^ to this user.',

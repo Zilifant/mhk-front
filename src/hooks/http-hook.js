720f7d51cf5ee92aea1c1caf20b7a7f689697748
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-07 16:51:31 -0500

    add/edit comments

--- src/hooks/http-hook.js
+++ src/hooks/http-hook.js
@@ -1,4 +1,4 @@
-// HTTP hook
+// HTTP Hook
 
 import { useState, useCallback, useRef, useEffect } from 'react';
 import { isDevEnv } from '../util/utils';
@@ -6,18 +6,18 @@ import { isDevEnv } from '../util/utils';
 export const useHttpClient = (caller) => {
   if (isDevEnv) console.log(`HOOK: useHttpClient (${caller || '?'})`);
 
-  const [isLoading, setIsLoading] = useState(false); // loading state
-  const [error, setError] = useState(undefined); // error state
+  const [isLoading, setIsLoading] = useState(false); // Loading state
+  const [error, setError] = useState(undefined); // Error state
 
-  // prevent errors if page is switched while this is loading, where we would
+  // Prevent errors if page is switched while this is loading, where we would
   // try to update the state of a component not on the screen anymore cancel
   // the ongoing http request using useRef hook it will turn into a reference:
-  // data that will not change / be re-initialized when the page re-renders
+  // data that will not change / be re-initialized when the page re-renders.
   const activeHttpRequests = useRef([]);
 
-  // wrap the entire thing in `useCallback` to avoid an infinite loop (or merely
-  // an inefficient re-render cycle) if/when the component that calls this hook
-  // re-renders and would call this function again
+  // Wrap the entire thing in `useCallback` to avoid an infinite loop (or
+  // merely an inefficient re-render cycle) if/when the component that calls
+  // this hook re-renders and would call this function again.
   const sendRequest = useCallback(async (
     url,
     method = 'GET',
@@ -25,13 +25,13 @@ export const useHttpClient = (caller) => {
     headers = {}
   ) => {
 
-    setIsLoading(true); // to enable loading indicator in UX
+    setIsLoading(true); // Indicate when the Loading component should render.
 
     const httpAbortCtrl = new AbortController();
     activeHttpRequests.current.push(httpAbortCtrl);
 
     try {
-      // forward the params to `fetch`
+      // Forward the args to `fetch`.
       const response = await fetch(url, {
         method,
         body,
@@ -40,32 +40,30 @@ export const useHttpClient = (caller) => {
         credentials: 'include'
       });
 
-      const responseData = await response.json(); // should be the data
+      const responseData = await response.json(); // Should be the data.
 
-      // if request was successful, filter out the specific AbortController for
-      // this request; we don't try to cancel a request that already completed
+      // If request was successful, filter out the specific AbortController for
+      // this request; we don't try to cancel a request that already completed.
       activeHttpRequests.current = activeHttpRequests.current.filter(
         reqCtrl => reqCtrl !== httpAbortCtrl
         );
 
-      // error may result in no response; but a 'successful' response may also
-      // bring an error; to catch these errors use `ok`
-      // `ok` property of response object is true for 200ish status codes,
-      // false for 4-500ish codes
+      // Error may result in no response; but a 'successful' response may also
+      // bring an error; to catch these errors, use `ok`.
       if (!response.ok) {
         console.log(responseData.message);
         throw new Error(responseData.message);
       }
 
       setIsLoading(false);
-      // return the data to the component that is using this hook
+      // Return the data to the component that is using this hook.
       return responseData;
 
     } catch (err) {
       setError(err.message || 'Http-hook: something went wrong.');
       setIsLoading(false);
-      // throw error, so that the component using this hook knows that something
-      // went wrong and to not continue
+      // Throw error, so that the component using this hook knows that
+      // something went wrong and to not continue.
       throw err;
     }
   }, []);
@@ -74,7 +72,7 @@ export const useHttpClient = (caller) => {
     setError(null);
   };
 
-  // cleanup function
+  // Cleanup.
   useEffect(() => {
     return () => {
       activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());

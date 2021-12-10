c9443350c6b9b16b93e980343c564dcda28feecd
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-09 19:55:56 -0500

    add comments

--- src/components/shared/Tooltip.jsx
+++ src/components/shared/Tooltip.jsx
@@ -1,4 +1,12 @@
-// Tooltip
+// Tooltip //
+// Small textbox that displays while another element is hovered; displays
+// adjacent to that element.
+
+// `side` prop defines what side of hovered element tip will appear on.
+// (`top`, `bottom`, `left`, `right`; defaults to `bottom`.)
+
+// `opts` prop defines whether text should wrap.
+// (`singleline` for short tips; defaults to wrapping text.)
 
 import { parse, render } from '../../util/smd';
 import { tooltipText } from '../../util/tooltip-text';
@@ -8,7 +16,7 @@ function renderTooltip(tip, side, opts) {
   if (!tooltipText[tip]) return null;
   const parsedTip = parse(tooltipText[tip]);
   return (
-    <div className={(`ttip ${side} ${opts || ''}`).trim()}>
+    <div className={(`ttip ${side || 'bottom'} ${opts || 'wrap'}`).trim()}>
       {render.block(parsedTip)}
     </div>
   );

f63a3bb11b18c16ed7774e1ed63e0a352f46e38d
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-14 19:18:27 -0500

    add comments

--- src/hooks/chat-hook.js
+++ src/hooks/chat-hook.js
@@ -39,32 +39,47 @@ export const useChat = (chat) => {
 
   // Used by Chat and Announcer //
 
+  // Determine what message to show when the Announcer component first renders
+  // and there is not a game active.
   const lastAnnouncement = () => {
+    // Check for the most recently saved system message.
     const last = messages.filter(m => m.type !== 'userMessage').slice(-1)[0];
+    // Structure welcome message data for `buildSMDString` module.
+    // TO DO: Move this elsewhere.
     const welcome = {
       time: new Date().toLocaleTimeString().slice(0,-6),
       type: 'welcome',
       args: [],
       senderId: 'app'
     };
+    // If a saved system message was found, return that, else return the
+    // welcome message.
     return !!last ? last : welcome;
   };
 
+  // Determine what message to show when the Announcer component first renders
+  // and there is an active game.
   const lastGameAnnouncement = () => {
+    // Get the most recently saved in-game system message.
     const lga = messages.filter(m => !!m.isInGame).slice(-1)[0];
-    if (!!lga) return lga;
-    return null;
+    // This should always find a message; but if not, return null.
+    return !!lga ? lga : null;
   };
 
   // TO DO: Is useCallBack necessary here?
   const subToAnnounce = useCallback(() => {
 
+    // Subscribe to general announcements.
     const subToPublicAnnounce = () => {
       socket.current.on('updateLobby', ({ msg }) => {
+        // Not all `updateLobby` events send a message.
         if (msg) return setMessages((messages) => [...messages, msg]);
       });
     };
 
+    // Subscribe to announcements only sent to specific clients.
+    // Currently only used to send a notification when a user attempts to join
+    // a lobby in a second browser window/tab with the same cookie data.
     const SubToPrivateAnnounce = () => {
       socket.current.on('privateAnnounce', msg => {
         return setMessages((messages) => [...messages, msg]);

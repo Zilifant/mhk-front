0cd20b06e9fd682e919abf8ee1e837d14863dabd
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-15 14:53:31 -0500

    add comment

--- src/components/modal/Backdrop.jsx
+++ src/components/modal/Backdrop.jsx
@@ -1,8 +1,11 @@
+// Backdrop //
+// Used with ErrorModal. (Not InfoModal or VideoModal.)
+
 import ReactDOM from 'react-dom';
 
 const Backdrop = props => {
   return ReactDOM.createPortal(
-    <div className="backdrop" onClick={props.onClick}></div>,
+    <div className='backdrop' onClick={props.onClick}></div>,
     document.getElementById('backdrop-hook')
   );
 };
